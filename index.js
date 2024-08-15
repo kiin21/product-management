129e8a479cba1b0cd5dd30c8778f41b6e9a0f526
kiin21 <pdanhkhoa21@gmail.com>
2024-08-15 15:59:56 +0700

    add .env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = require('./routers/client/index.router.js');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -11,7 +12,6 @@ router(app);
 // app.get('/', (req, res) => {
 //     res.render("client/pages/home/index");
 // });
-
 
 // app.get('/products', (req, res) => {
 //     res.render("client/pages/products/index");

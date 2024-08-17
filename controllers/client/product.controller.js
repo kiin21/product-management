658aa029f0e356f33e79b8e0da3d955c74d3f1c1
kiin21 <pdanhkhoa21@gmail.com>
2024-08-17 11:01:33 +0700

    Add products list on clien view

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,15 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    let products = await Product.find({});
+
+    const newProducts = products.map(element => {
+        element.newPrice = (element.price / (1 - element.discountPercentage / 100)).toFixed(2);
+        return element;
+    });
+
     res.render("client/pages/products/index.pug", {
-        pageTitle: "Products"
+        pageTitle: "Products",
+        products: newProducts
     });
 }

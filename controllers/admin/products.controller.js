53c8bfa1d1874460131d724fefa08626c632181e
kiin21 <pdanhkhoa21@gmail.com>
2024-08-19 22:19:33 +0700

    Retrive data from MongoDB and display to admin product section

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -2,6 +2,21 @@ const Product = require("../../models/product.model");
 
 module.exports.products = async (req, res) => {
     let products = await Product.find({});
+
+    console.log(products);
+
+    products.forEach(element => {
+        if (element.stock === 0) {
+            element.status = "out of stock";
+        }
+        else if (element.stock < 10) {
+            element.status = "low stock";
+        }
+        else {
+            element.status = "in stock";
+        }
+    });
+
     res.render("admin/pages/products/index", {
         products: products
     });

fdc23ac34ed95a4f1859370cf8db1da269ec8d79
kiin21 <pdanhkhoa21@gmail.com>
2024-09-12 13:20:10 +0700

    Fix some mistype word

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('Connected to database');
     } catch (error) {
         console.log('Error connecting to database');

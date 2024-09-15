797af0b4c6df03131b91a816d241b0bbc231f980
kiin21 <pdanhkhoa21@gmail.com>
2024-09-15 21:09:12 +0700

    Fix router error

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -16,12 +16,36 @@ module.exports.index = async (req, res) => {
     });
 }
 
-// [get] /admin/product-category/create
-module.exports.create = (req, res) => {
-    res.render("admin/pages/product-category/create", {
-        pageTitle: "Create Product Category"
+const createChild = (records, category) => {
+    category.child = []; // Initialize the child array once
+    if (category.parent_id === '') {
+        category.level = 0;
+    }
+
+    records.forEach(record => {
+        if (String(record.parent_id) === String(category._id)) {
+            category.child.push(record);
+            record.level = category.level + 1;
+            createChild(records, record);
+        }
     });
 }
+
+// [get] /admin/product-category/create
+module.exports.create = async (req, res) => {
+    let filter = { deleted: false };
+    let records = await ProductCategory.find(filter);
+
+    records.forEach(record => { createChild(records, record) });
+
+    records.forEach(record => console.log(record.level));
+
+    res.render("admin/pages/product-category/create", {
+        pageTitle: "Create Product Category",
+        records: records
+    });
+}
+
 // [post] /admin/product-category/create
 module.exports.createPost = async (req, res) => {
     req.body.parent_id = String(req.body.parent_id);

24c2988062f1405e02331687c442301d0dd1074c
kiin21 <pdanhkhoa21@gmail.com>
2024-09-20 17:56:30 +0700

    Add detail page

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -15,14 +15,28 @@ module.exports.index = async (req, res) => {
     });
 }
 
-// [get] /products/:slug
+// [get] /products/detail/:slugProduct
 module.exports.detail = async (req, res) => {
     try {
         let product = await Product.findOne(
             {
-                slug: req.params.slug,
+                slug: req.params.slugProduct,
                 deleted: false,
             });
+
+        if (product.product_category_id) {
+            const category = await ProductCategory.findOne({
+                _id: product.product_category_id,
+                status: 'available',
+                deleted: false
+            });
+
+            product.category = category;
+            console.log('CATEGORY', product);
+        }
+
+        const newProduct = productHelper.fixedPrice([product])[0];
+        product.newPrice = newProduct.newPrice;
 
         res.render("client/pages/products/detail.pug", {
             pageTitle: "Product detail",
@@ -43,7 +57,7 @@ module.exports.category = async (req, res) => {
     });
 
     const categories = await ProductCategory.find({ deleted: false, status: 'available' });
-    const descendant_category = []; 
+    const descendant_category = [category._id];
     for (const item of categories) {
         if (await isDescendant(item, category._id)) {
             descendant_category.push(item._id);

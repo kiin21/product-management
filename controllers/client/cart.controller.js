043f92812c71f0a352e030c7d132e1e3c5ffd12e
kiin21 <pdanhkhoa21@gmail.com>
2024-09-23 21:36:19 +0700

    Add cart page

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,7 +1,31 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
 const helper = require("../../helpers/product");
 
+// [get] /cart
+module.exports.index = async (req, res) => {
+    const cartId = req.cookies.cartId;
+    const cart = await Cart.findOne({ _id: cartId });
 
+    if (cart.products.length > 0) {
+        for (item of cart.products) {
+            const productId = item.product_id;
+            const product = await Product.findOne({
+                _id: productId
+            }).select("title thumbnail slug price discountPercentage");
+
+            product.newPrice = helper.fixedPrice([product])[0].newPrice;
+
+            item.productInfo = product;
+            item.total = product.newPrice * item.quantity;
+        }
+        cart.total = cart.products.reduce((total, item) => total + item.total, 0);
+        res.render("client/pages/cart/index",{
+            cart: cart
+        });
+    };
+
+}
 // [post] /cart/add/:productId
 module.exports.add = async (req, res) => {
     const productId = req.params.productId;
--- helpers/product.js
+++ helpers/product.js
@@ -1,6 +1,6 @@
 module.exports.fixedPrice = (products) => {
     const newProducts = products.map(element => {
-        element.newPrice = (element.price / (1 - element.discountPercentage / 100)).toFixed(2);
+        element.newPrice = parseInt((element.price * (1 - element.discountPercentage / 100)).toFixed(2));
         return element;
     });
 

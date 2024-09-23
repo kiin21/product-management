d46e686760de80fc8344d796f3250e799cd07a04
kiin21 <pdanhkhoa21@gmail.com>
2024-09-23 23:31:32 +0700

    Fix alert message

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,19 @@
-// // alert message
-// //show
-// const alertMessage = document.querySelector("#alert-popup");
-// if (alertMessage) {
-//     let timeout = alertMessage.getAttribute("timeout");
-//     setTimeout(() => {
-//         alertMessage.classList.add("alert-hidden");
-//     }, timeout);
-// }
-// //close
-// const closeAlert = document.querySelector("[close-alert]");
-// if (closeAlert) {
-//     closeAlert.addEventListener("click", () => {
-//         alertMessage.classList.add("alert-hidden");
-//     });
-// }
-// // end alert message
+// alert message
+//show
+const alertMessage = document.querySelector("#alert-popup");
+if (alertMessage) {
+    let timeout = alertMessage.getAttribute("timeout");
+    setTimeout(() => {
+        alertMessage.classList.add("alert-hidden");
+        console.log("Timeout, alert message hidden");
+        console.log(alertMessage.classList.contains("alert-hidden"));
+    }, timeout);
+}
+//close
+const closeAlert = document.querySelector("[close-alert]");
+if (closeAlert) {
+    closeAlert.addEventListener("click", () => {
+        alertMessage.classList.add("alert-hidden");
+    });
+}
+// end alert message
